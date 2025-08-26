b4ef6846765f780f25a71d6d6ddaddd1e46f452c
Arox315 <127292374+Arox315@users.noreply.github.com>
2025-08-26 23:32:04 +0200

    feat: Update character structure and enable physics interpolation to remove jitter. Add MultipalyerSynchronizers to physics nodes for improved multiplayer functionality and to remove physics interpolation warnings

--- entities/character/character.gd
+++ entities/character/character.gd
@@ -8,10 +8,13 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const CAMERA_ROTATION_SPEED = 50.0 * deg_to_rad(0.022)
 
-@onready var neck := $Neck
-@onready var camera: Camera3D = $Neck/Camera3D
+@onready var camera_controller: Node3D = $CameraController
+@onready var camera: Camera3D = $CameraController/Camera3D
+@onready var camera_controller_anchor: Marker3D = $CameraControllerAnchor
+@onready var interact_ray: InteractRay = $CameraControllerAnchor/InteractRay
 @onready var world_model: Node3D = $WorldModel
 @onready var body_model: MeshInstance3D = $WorldModel/BodyModel
+@onready var world_model_anchor: Marker3D = $WorldModel/WorldModelAnchor
 @onready var player_type: Control = $Hud/PlayerType
 
 var direction:Vector3 = Vector3.ZERO
@@ -19,11 +22,17 @@ var is_third_person: bool = false
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
+	if get_tree().current_scene.get_node("PlayersSpawnPoint"):
+		position = get_tree().current_scene.get_node("PlayersSpawnPoint").global_position
 
 
 func _ready() -> void:
 	# Don't execute code if not authority
 	if not is_multiplayer_authority():
+		# disable camera if not authority
+		camera.set_physics_interpolation_mode(Node.PHYSICS_INTERPOLATION_MODE_OFF)
+		camera.set_process(false)
+		camera.set_physics_process(false)
 		return
 	
 	# add palyer type to hud
@@ -36,12 +45,37 @@ func _ready() -> void:
 	camera.position = first_person_camera_position
 	camera.current = is_multiplayer_authority()
 	
+	# Set camera controller settings for interpolation
+	camera_controller.top_level = true
+	camera_controller.set_physics_interpolation_mode(Node.PHYSICS_INTERPOLATION_MODE_OFF)
+	
 	# Set random color for 3rd-person mesh
 	character_color = Color(RandomColorGenerator.rand_hex())
 	body_model.color = character_color
 	
 	# Hide authority's world model
 	hide_world_model()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	# Don't execute code if not authority
+	if not is_multiplayer_authority():
+		return
+		
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			rotate_y(deg_to_rad(-event.relative.x * CAMERA_ROTATION_SPEED))
+			
+			camera.rotate_x(deg_to_rad(-event.relative.y * CAMERA_ROTATION_SPEED))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(90))
+			
+			interact_ray.rotate_x(deg_to_rad(-event.relative.y * CAMERA_ROTATION_SPEED)) 
+			interact_ray.rotation.x = clamp(interact_ray.rotation.x, deg_to_rad(-60), deg_to_rad(90))
 
 
 func _physics_process(delta: float) -> void:
@@ -61,35 +95,23 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("left", "right", "forward", "back").normalized()
+	direction = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	# Don't execute code if not authority
-	if not is_multiplayer_authority():
-		return
-		
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			neck.rotate_y(deg_to_rad(-event.relative.x * CAMERA_ROTATION_SPEED))
-			camera.rotate_x(deg_to_rad(-event.relative.y * CAMERA_ROTATION_SPEED))
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(90))
+func _process(_delta: float) -> void:
+	camera_controller.global_position = camera_controller_anchor.get_global_transform_interpolated().origin
+	body_model.global_position = world_model_anchor.get_global_transform_interpolated().origin
+	global_basis = global_basis.orthonormalized()
+	camera_controller.global_basis = global_basis.orthonormalized()
 
 
 func toggle_third_person() -> void:
@@ -101,13 +123,12 @@ func toggle_third_person() -> void:
 		camera.position = third_person_camera_position
 		show_world_model()
 		is_third_person = true
-		
 
 
 func hide_world_model() -> void:
 	if not is_instance_valid(world_model):
 		return
-	for child: MeshInstance3D in world_model.get_children():
+	for child in world_model.get_children():
 		if child is MeshInstance3D:
 			child.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_SHADOWS_ONLY
 
@@ -116,6 +137,6 @@ func show_world_model() -> void:
 	if not is_instance_valid(world_model):
 		return
 		
-	for child: MeshInstance3D in world_model.get_children():
+	for child in world_model.get_children():
 		if child is MeshInstance3D:
 			child.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
